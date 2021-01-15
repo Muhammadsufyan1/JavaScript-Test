ec73663b1e9e2486d3d9652e4aa7913ca542e5c8
Muhammad Sufyan <muhammadsufyan666@gmail.com>
2021-01-15 22:52:31 +0500

    Task Completed Multiple Ways
    I have complete this taks in different ways like OOP, Core JS and jQuery

--- search.js
+++ search.js
@@ -1,24 +1,46 @@
+// Using OOP
 class search{
 
-  //var key; var value;	
+  //var key; var value; 
+  
   constructor(key,value) {
     this.key = key;
     this.value = value;
   }
+
   setStorageValue() {
     localStorage.setItem(this.key,this.value);
     return "Item Save";
   }
+  
   getStorageValue(){
-  	return localStorage.getItem(this.key);
+    return localStorage.getItem(this.key);
   }
+
 }
+
 document.addEventListener("DOMContentLoaded", function(event) { 
+
    var val = document.querySelectorAll('[itemprop="sku"]')[0];
-	var searchObj = new search("SKU",val.innerHTML);
-	var setItem = searchObj.setStorageValue();
-	var getItem = searchObj.getStorageValue();
-	console.log(getItem+' '+setItem);
+  var searchObj = new search("SKU",val.innerHTML);
+  var setItem = searchObj.setStorageValue();
+  var getItem = searchObj.getStorageValue();
+  console.log(getItem+' '+setItem);
+  
 });
 
 
+// Using Core JS
+
+// document.addEventListener("DOMContentLoaded", function(event) { 
+// localStorage.setItem('SKU',document.querySelectorAll('[itemprop="sku"]')[0]);
+// console.log(localStorage.getItem('SKU'));
+//});
+
+// Using jQuery
+
+// $(document).ready(function(){
+//   localStorage.setItem("SKU", $("[itemprop="sku"]").html())
+//   console.log(localStorage.getItem('SKU'));
+
+// });
